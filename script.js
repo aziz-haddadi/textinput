a9a8d2455f45f6dd70d6730d81d261572dd74862
aziz-haddadi <mohamedaziz.haddadi@insat.ucar.tn>
2025-02-14 23:12:04 +0100

    text-input

---
+++ script.js
@@ -0,0 +1,51 @@
+const number = document.getElementById("number");
+const color = document.getElementById("color");
+const select = document.getElementById("fonts");
+const textarea = document.getElementById("area");
+
+
+const savedFontSize = localStorage.getItem("fontSize");
+const savedColor = localStorage.getItem("color");
+const savedFontFamily = localStorage.getItem("fontFamily");
+const savedText = localStorage.getItem("text");
+
+
+if (savedFontSize) {
+    textarea.style.fontSize = savedFontSize;
+    number.value = parseInt(savedFontSize, 10);
+}
+if (savedColor) {
+    textarea.style.color = savedColor;
+    color.value = savedColor;
+}
+if (savedFontFamily) {
+    textarea.style.fontFamily = savedFontFamily;
+    select.value = savedFontFamily;
+}
+if (savedText) {
+    textarea.value = savedText;
+}
+
+
+number.addEventListener("input", () => {
+    const fontSize = number.value + 'px';
+    textarea.style.fontSize = fontSize;
+    localStorage.setItem("fontSize", fontSize);
+});
+
+
+color.addEventListener("input", () => {
+    const textColor = color.value;
+    textarea.style.color = textColor;
+    localStorage.setItem("color", textColor);
+});
+
+select.addEventListener("change", () => {
+    const fontFamily = select.value;
+    textarea.style.fontFamily = fontFamily;
+    localStorage.setItem("fontFamily", fontFamily);
+});
+
+textarea.addEventListener("input", () => {
+    localStorage.setItem("text", textarea.value);
+});
